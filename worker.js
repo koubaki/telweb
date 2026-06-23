3c8058c75fb1257396449f4f6896c16d5e0739f3
koubaki <koutavaki1000@gmail.com>
2026-06-23 13:18:19 +0300

    init

--- worker.js
+++ worker.js
@@ -2,6 +2,26 @@ export default {
   async fetch(request, env, ctx) {
     const url = new URL(request.url)
 
-    return env.ASSETS.fetch(request)
-  },
+    if (request.method === 'OPTIONS') {
+      return new Response(null, {
+        status: 204,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+          'Access-Control-Allow-Headers': request.headers.get('Access-Control-Request-Headers') || 'Content-Type, Authorization',
+          'Access-Control-Max-Age': '86400'
+        }
+      })
+    }
+
+    const response = await env.ASSETS.fetch(request)
+
+    const newResponse = new Response(response.body, response)
+
+    newResponse.headers.set('Access-Control-Allow-Origin', '*')
+    newResponse.headers.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+    newResponse.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+
+    return newResponse
+  }
 }
